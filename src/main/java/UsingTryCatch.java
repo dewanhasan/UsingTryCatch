6ca116b0b14cba61aabb48ad2839a0c46b2bef45
dewanhasan <g00410156@atu.ie>
2022-11-08 13:33:13 +0000

    Added new info

---
+++ src/main/java/UsingTryCatch.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UsingTryCatch {
+    public static void main(String[] args) {
+        System.out.println("please enter a number between 0 - 9");
+        Scanner myscan = new Scanner(System.in);
+        int myNum = myscan.nextInt();
+        if (myNum <= 9 && myNum > 0){
+            System.out.println("You entered " + myNum);
+        }
+        else{
+            System.out.println("Not a valid number!");
+        }
+    }
+}
